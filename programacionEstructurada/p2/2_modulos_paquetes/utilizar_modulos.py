5d56c2864c6abbc6f04e60a10354f6932a03f3a7
eliza3042250085-ai <eliza_3042250085@utd.edu.mx>
2026-06-11 18:17:26 -0600

    utilizar_modulos.py

--- programacionEstructurada/p2/2_modulos_paquetes/utilizar_modulos.py
+++ programacionEstructurada/p2/2_modulos_paquetes/utilizar_modulos.py
@@ -1,18 +1,16 @@
-# 1er utilizar los modulos 
-#import modulos
-#modulos.borrarPantalla()
+1er utilizar los modulos 
+import modulos
+modulos.borrarPantalla()
 
-#n="Daniel"
-#a="Carreón"
+n="Daniel"
+a="Carreón"
 
-#nombre, apellidos=modulos.funcion4(n,a)
-#print(f"El nombre completo es: {nombre } {apellidos}")
+nombre, apellidos=modulos.funcion4(n,a)
+print(f"El nombre completo es: {nombre } {apellidos}")
 
-
-#2da formar de utilizar modulos
+2da formar de utilizar modulos
 
 from modulos import borrarPantalla,funcion3,funcion4
-
 
 borrarPantalla()
 n="Daniel"
